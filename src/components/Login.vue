309139987326bbf9fa6553e05fd8931dee16bcb4
zwj <644893530@qq.com>
2022-02-11 00:32:52 +0800

    login1

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Login组件
+    <!-- 登录表单区域 -->
+    <el-form label-width="80px">
+      <el-form-item label="username"></el-form-item>
+      <el-input></el-input>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+ 
+</style>
